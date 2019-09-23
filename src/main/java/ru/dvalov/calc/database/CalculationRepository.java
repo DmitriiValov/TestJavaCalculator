5e260dec48d661fd2bd5b2bfce037a147b09295c
Dmitry Valov <d_valov@mail.ru>
2019-09-23 14:17:51 +0600

    Исправил ошибку в запросах для операций.

--- src/main/java/ru/dvalov/calc/database/CalculationRepository.java
+++ src/main/java/ru/dvalov/calc/database/CalculationRepository.java
@@ -11,10 +11,10 @@ import java.util.List;
 @Repository
 public interface CalculationRepository extends JpaRepository<Calculation, Integer> {
 
-    @Query(value = "select c.expression from Calculation c inner join c.operations as op where op.type = ?1", nativeQuery = false)
+    @Query(value = "select distinct c.expression from Calculation c inner join c.operations as op where op.type = ?1", nativeQuery = false)
     List<String> findExpressionsWithOperation(@Param("operationId") int operation);
 
-    @Query(value = "select count(c.expression) from Calculation c inner join c.operations as op where op.type = ?1", nativeQuery = false)
+    @Query(value = "select count(distinct c.expression) from Calculation c inner join c.operations as op where op.type = ?1", nativeQuery = false)
     int getExpressionsCountWithOperation(@Param("operationId") int operation);
 
     @Query(value = "select c.expression from Calculation c where c.date between :dateBegin and :DateEnd", nativeQuery = false)
